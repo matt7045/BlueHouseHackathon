6993afa44ccae243773dfeba42f011ba0d001a65
Matthew Abazis <mattu1582@gmail.com>
2021-01-17 15:56:04 -0500

    Updated gitignore and removed old credentials

--- config/alpaca.py
+++ config/alpaca.py
@@ -2,5 +2,5 @@ PAPER_TRADE_URL = 'https://paper-api.alpaca.markets'
 PAPER_TRADE_API_VERSION = 'v2'
 
 # Fill in your Alpaca API credentials
-API_KEY = 'PK0BLEZ6QDB1783HBZ2L'
-SECRET_KEY = 'pUuXlmifIbnfqr9AHk48Z9Q6Dq7jCtPHd8XKCjti'
+API_KEY = ''
+SECRET_KEY = ''
